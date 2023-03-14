7a3fccffcbb2fd5657401e4a4bd3be5c909757a8
Ben Barton <bsb117@student.bham.ac.uk>
2023-03-14 18:54:30 +0000

    Bens attempt to fix things

--- src/backend/backEnd/interface/interface_song.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.bham.teamproject.web.rest.backEnd.repository;
-
-import uk.ac.bham.teamproject.web.rest.backEnd.model;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-
-}
