3f84780c0c13ed841e132a72f91d79acd283a224
Charlie <cxh162@student.bham.ac.uk>
2023-03-17 14:07:46 +0000

    added import for sql containers because I didn't have a jar file

--- src/test/java/uk/ac/bham/teamproject/config/SqlTestContainer.java
+++ src/test/java/uk/ac/bham/teamproject/config/SqlTestContainer.java
@@ -2,6 +2,7 @@ package uk.ac.bham.teamproject.config;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public interface SqlTestContainer extends InitializingBean, DisposableBean {
