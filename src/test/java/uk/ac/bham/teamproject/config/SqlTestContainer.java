77586619209dba404e00650a57188f9b9aeabe05
Charlie Hill <cxh162@student.bham.ac.uk>
2023-03-19 12:21:36 +0000

    I think this fixes the sqlcontainer issues, for some reasont there original file was changed

--- src/test/java/uk/ac/bham/teamproject/config/SqlTestContainer.java
+++ src/test/java/uk/ac/bham/teamproject/config/SqlTestContainer.java
@@ -1,10 +1,23 @@
+/*
 package uk.ac.bham.teamproject.config;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.*;
 import org.testcontainers.containers.PostgreSQLContainer;
 
 public interface SqlTestContainer extends InitializingBean, DisposableBean {
     PostgreSQLContainer<?> getTestContainer();
+}
+*/
+
+package uk.ac.bham.teamproject.config;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+
+public interface SqlTestContainer extends InitializingBean, DisposableBean {
+    JdbcDatabaseContainer<?> getTestContainer();
 }
