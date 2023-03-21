51b4e32674613d22743ea1420199ee1bb3ce05ef
Charlie <cxh162@student.bham.ac.uk>
2023-03-21 14:45:38 +0000

    added files for liked song component and routing

---
+++ src/main/webapp/app/menu/liked-songs/liked-songs.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-liked-songs',
+  templateUrl: './liked-songs.component.html',
+  styleUrls: ['./liked-songs.component.scss'],
+})
+export class LikedSongsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
