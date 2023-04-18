8fa70de1e945f43e57a7bf96b69a8a39a43f324a
HanBin Leow <hxl220@student.bham.ac.uk>
2023-04-18 23:02:18 +0100

    added profile page

--- src/main/webapp/app/menu/profile/profile.component.ts
+++ src/main/webapp/app/menu/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 @Component({
   selector: 'jhi-profile',
   templateUrl: './profile.component.html',
@@ -8,5 +9,51 @@ import { Component, OnInit } from '@angular/core';
 export class ProfileComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    var myChart = new Chart('myChart', {
+      type: 'pie',
+      data: {
+        labels: ['Ariana Grande', 'Justin Bieber', 'Adele'],
+        datasets: [
+          {
+            label: 'Songs',
+            data: [12, 19, 3],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+
+    var genreChart = new Chart('genreChart', {
+      type: 'pie',
+      data: {
+        labels: ['Pop', 'Rock', 'Romantic'],
+        datasets: [
+          {
+            label: '# of Songs',
+            data: [21, 15, 5],
+            backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)'],
+            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+        },
+      },
+    });
+  }
 }
