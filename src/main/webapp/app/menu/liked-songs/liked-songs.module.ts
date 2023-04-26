7d0c2627439a7f696fd3d166c3ebea8dd9c1d66f
XamooriX20 <amori2002@icloud.com>
2023-04-26 14:40:39 +0100

    Omar added the code for the liked songs list

--- src/main/webapp/app/menu/liked-songs/liked-songs.module.ts
+++ src/main/webapp/app/menu/liked-songs/liked-songs.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'; // Add this import
 
-import { LikedSongsComponent } from './liked-songs.component';
 import { likedSongsRoute } from './liked-songs.route';
+import { LikedSongsComponent } from './liked-songs.component';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([likedSongsRoute])],
+  imports: [SharedModule, RouterModule.forChild([likedSongsRoute]), FontAwesomeModule], // Add FontAwesomeModule to the imports array
   declarations: [LikedSongsComponent],
 })
-export class LikedSongsModule {}
+export class LikedSongsModule {} // Define the LikedSongsModule separately
