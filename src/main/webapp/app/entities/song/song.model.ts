517f2474ae6d059f7ebf5f14723dd81319857a55
Charlie <cxh162@student.bham.ac.uk>
2023-05-01 14:46:59 +0100

    removed the changes to song model

--- src/main/webapp/app/entities/song/song.model.ts
+++ src/main/webapp/app/entities/song/song.model.ts
@@ -3,12 +3,12 @@ import { IUser } from 'app/entities/user/user.model';
 export interface ISong {
   id: number;
   spotifySongId?: string | null;
+
+  //songName = liked:boolean
   songName?: string | null;
   spotifyArtistId?: string | null;
   artistName?: string | null;
   user?: Pick<IUser, 'id' | 'login'> | null;
-  isLiked?: boolean;
-  isDisliked?: boolean;
 }
 
 export type NewSong = Omit<ISong, 'id'> & { id: null };
