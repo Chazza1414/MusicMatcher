0488d8001c25c3d7b9c384800f7c6b8c6aeb2425
XamooriX20 <amori2002@icloud.com>
2023-05-02 23:34:30 +0100

    Play/Stop Button working for liked songs

--- src/main/webapp/app/menu/liked-songs/liked-songs.component.ts
+++ src/main/webapp/app/menu/liked-songs/liked-songs.component.ts
@@ -11,35 +11,19 @@ import { InitialTrainingComponent } from '../../initial-training/initial-trainin
 })
 /* eslint-disable @typescript-eslint/member-ordering */
 export class LikedSongsComponent implements OnInit {
+  public songInfoArray: any[] = [];
   currentlyPlayingIndex: number | null = null;
   //private likedSongsSubscription: Subscription;
 
-  currentSong: any;
+  currentSong: { title: string; artist: string } | null = null;
   progress: number = 0;
 
-  public songInfoArray: any[] = [];
+  //private playingAudioElements: any[];
+  private currentAudio: HTMLAudioElement | null = null;
 
   isPlaying(index: number): boolean {
     return this.currentlyPlayingIndex === index;
   }
-
-  /*songs: ISong[] = [
-    /!*{ id: 1, spotifySongId: null, songName: 'Song 1', spotifyArtistId: null, artistName: 'Artist 1', user: null },
-    { id: 2, spotifySongId: null, songName: 'Song 2', spotifyArtistId: null, artistName: 'Artist 2', user: null },
-    { id: 3, spotifySongId: null, songName: 'Song 3', spotifyArtistId: null, artistName: 'Artist 3', user: null },
-    { id: 4, spotifySongId: null, songName: 'Song 4', spotifyArtistId: null, artistName: 'Artist 4', user: null },
-    { id: 5, spotifySongId: null, songName: 'Song 5', spotifyArtistId: null, artistName: 'Artist 5', user: null },
-    { id: 6, spotifySongId: null, songName: 'Song 6', spotifyArtistId: null, artistName: 'Artist 6', user: null },
-    { id: 7, spotifySongId: null, songName: 'Song 7', spotifyArtistId: null, artistName: 'Artist 7', user: null },
-    { id: 8, spotifySongId: null, songName: 'Song 8', spotifyArtistId: null, artistName: 'Artist 8', user: null },
-    { id: 9, spotifySongId: null, songName: 'Song 9', spotifyArtistId: null, artistName: 'Artist 9', user: null },
-    { id: 10, spotifySongId: null, songName: 'Song 10', spotifyArtistId: null, artistName: 'Artist 10', user: null },
-    { id: 11, spotifySongId: null, songName: 'Song 11', spotifyArtistId: null, artistName: 'Artist 11', user: null },
-    { id: 12, spotifySongId: null, songName: 'Song 12', spotifyArtistId: null, artistName: 'Artist 12', user: null },
-    { id: 13, spotifySongId: null, songName: 'Song 13', spotifyArtistId: null, artistName: 'Artist 13', user: null },
-    { id: 14, spotifySongId: null, songName: 'Song 14', spotifyArtistId: null, artistName: 'Artist 14', user: null },
-    { id: 15, spotifySongId: null, songName: 'Song 15', spotifyArtistId: null, artistName: 'Artist 15', user: null }*!/
-  ]; //Dummy data*/
 
   constructor(private songService: SongService, private initialComponent: InitialTrainingComponent) {}
 
@@ -66,6 +50,7 @@ export class LikedSongsComponent implements OnInit {
             artist: data.artists[0].name,
             album: data.album.name,
             genre: data.genre,
+            audio: data.preview_url,
           };
         });
     });
@@ -108,12 +93,37 @@ export class LikedSongsComponent implements OnInit {
     if (index === undefined || index === null) {
       this.currentlyPlayingIndex = null;
       this.currentSong = null;
+      if (this.currentAudio) {
+        this.currentAudio.pause();
+        this.currentAudio.currentTime = 0;
+      }
     } else if (this.currentlyPlayingIndex === index) {
       this.currentlyPlayingIndex = null;
       this.currentSong = null;
+      if (this.currentAudio) {
+        this.currentAudio.pause();
+        this.currentAudio.currentTime = 0;
+      }
     } else {
+      if (this.currentAudio) {
+        this.currentAudio.pause();
+        this.currentAudio.currentTime = 0;
+      }
       this.currentlyPlayingIndex = index;
-      this.currentSong = this.songInfoArray[index];
+      if (this.currentSong) {
+        // Add null check here
+        this.currentSong.title = this.songInfoArray[index].title;
+        this.currentSong.artist = this.songInfoArray[index].artist;
+      }
+      this.currentAudio = new Audio(this.songInfoArray[index].audio);
+      this.currentAudio.play().catch(error => {
+        console.log('Error playing audio:', error);
+      });
+      this.currentAudio.addEventListener('ended', () => {
+        this.currentlyPlayingIndex = null;
+        this.currentSong = null;
+        this.progress = 0;
+      });
     }
     this.progress = 0; // Initialize progress to 0
     // Update progress with a fake value for demonstration purposes
